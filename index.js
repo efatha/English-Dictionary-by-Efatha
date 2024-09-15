017077fa53d48bb440d5b250ec73f9a77e8cb042
efatha <163284022+efatha@users.noreply.github.com>
2024-09-15 13:07:45 +0200

    Added a nothing  display when a word is undefined

--- index.js
+++ index.js
@@ -13,15 +13,20 @@ btn.addEventListener("click", () =>{
     result.innerHTML = `
             <div class="word">
                 <h3>${inputword}</h3>
-                <button>
+                <button onclick="playSound()">
                     <i class="fa fa-volume-up" aria-hidden="true"></i>
                 </button>
             </div>
             <div class="details">
                 <p>${data[0].meanings[0].partOfSpeech}</p>
-                <p>${data[0].phonetic}</p>
+                <p>${data[0].phonetic || ""}</p>
             </div>
             <p class="word-meaning">${data[0].meanings[0].definitions[0].definition}</p>
-            <p class="word-example">${data[0].meanings[0].definitions[0].example || ""}</p>`
+            <p class="word-example">${data[0].meanings[0].definitions[0].example || ""}</p>`;
+            sound.setAttribute("src",`https:${data[0],phonetics[0].audio}`)
+          console.log(sound);
   });
 });
+function playSound(){
+  sound.play();
+}
