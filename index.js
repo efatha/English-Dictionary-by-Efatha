ad55106a28e578d870f568f7ba65a6bb2e2516cf
efatha <163284022+efatha@users.noreply.github.com>
2024-09-15 11:14:37 +0200

    fetch the URL and the input value #2
    This JS code will be fetching the result from the API URL when there are some value given to the input then it will generate the response .

--- index.js
+++ index.js
@@ -6,4 +6,10 @@ const btn = document.getElementById("search-btn");
 btn.addEventListener("click", () =>{
   let inputword = document.getElementById("inp-word").value;
   console.log(inputword);
+  fetch(`${url}${inputword}`)
+  .then((response)=> response.json())
+  .then((data) =>{
+    console.log(data);
+    result.innerHTML = ``
+  });
 });
