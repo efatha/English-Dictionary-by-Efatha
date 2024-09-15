cd75d6a345c011140ea119e86d5d888c1418f25e
efatha <163284022+efatha@users.noreply.github.com>
2024-09-15 12:31:09 +0200

    Cut the HTML properties in the result section and paste them into my index.js inner result #2
    In this stage, the result will be fetched and retrieved from the API based on the provided input value

--- index.js
+++ index.js
@@ -10,6 +10,18 @@ btn.addEventListener("click", () =>{
   .then((response)=> response.json())
   .then((data) =>{
     console.log(data);
-    result.innerHTML = ``
+    result.innerHTML = `
+            <div class="word">
+                <h3>${inputword}</h3>
+                <button>
+                    <i class="fa fa-volume-up" aria-hidden="true"></i>
+                </button>
+            </div>
+            <div class="details">
+                <p>${data[0].meanings[0].partOfSpeech}</p>
+                <p>${data[0].phonetic}</p>
+            </div>
+            <p class="word-meaning">${data[0].meanings[0].definitions[0].definition}</p>
+            <p class="word-example">${data[0].meanings[0].definitions[0].example || ""}</p>`
   });
 });
